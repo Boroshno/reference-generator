90374b40a9ba1168b5ba3f92429a5eeb60d188c3
Dmytro Lytvynenko <dmitry@myquest.co>
2020-05-16 09:37:44 +0300

    init comit. version 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+     
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ReferenceGenerator';
+export class AppComponent implements OnInit { 
+    bookForm : FormGroup;
+    reference : string;
+    errorMessage: string;
+    referenceList: string[] = [];
+    formValid: boolean;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+      ngOnInit() {
+        this.bookForm = this.formBuilder.group({
+            name: new FormControl(''),
+            surname: new FormControl(''),
+            chapter: new FormControl(''),
+            book: new FormControl('', Validators.required),
+            booktype: new FormControl(''),
+            city: new FormControl('', Validators.required),
+            publish: new FormControl('', Validators.required),
+            year: new FormControl('', Validators.required),
+            page: new FormControl('', Validators.required),
+            ISBN: new FormControl(''),
+            type: new FormControl('book'),
+            addAuthor: new FormArray([new FormControl(123)]),
+          });
+      
+        this.onChanges();
+      }
+
+      onChanges(): void {
+        this.bookForm.valueChanges.subscribe(val => {
+            this.formValid = this.bookForm.valid;
+            if (this.bookForm.valid)
+            {
+                this.errorMessage = "";
+                var chapter = val.chapter == '' ? '' : "— ${val.chapter} // ";
+                var ISBN = val.ISBN == '' ? '' : "— ${val.ISBN}.";
+                var bookType = val.bookType == '' ? '' : ": ${val.bookType} ";
+                this.reference =
+                `${val.surname}, ${val.name}  ${chapter}${val.book} [Текст] ${bookType}/ ${val.name} ${val.surname} — ${val.city} : ${val.publish}, ${val.year}. — ${val.page} с.${ISBN}`;
+            }
+            else
+            {
+                this.errorMessage = "Please, fill all the required fields to get the reference!";
+                this.reference = "";
+            }
+        });
+      }
+
+      changeType(e) {
+        alert("type changed")
+      }
+
+      addReference(): void {
+        if (this.reference != '' && this.formValid)
+        {
+            this.referenceList.push(this.reference);
+        }
+    }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent }   from './app.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
